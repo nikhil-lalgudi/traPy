402ef2f2077b6d5e9fe342adafff7b1388864782
Nikhil Lalgudi Vaidyanathan <144494028+nikhil-lalgudi@users.noreply.github.com>
2024-06-26 23:53:42 +0530

    Bridge

--- src/advanced statistical methods/stochastic calculus/continious processes/Brownian-Bridge.py
+++ src/advanced statistical methods/stochastic calculus/continious processes/Brownian-Bridge.py
@@ -1,4 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from error_handler import check_numeric, check_positive_integer, check_numeric_or_single_arg_callable, ensure_single_arg_constant_function
-from base import BaseTimeProcess
+
+from Brownian-Motion import BrownianMotion
+
+class BrownianBridge(BrownianMotion):
+    def __init__(self, b=0, t=1, rng=None):
+        super().__init__(drift=0, scale=1, t=t, rng=rng)
+        self.b = b
+
+    def __str__(self):
+        return f"Brownian bridge from 0 to {self.b} on [0, {self.t}]"
+
+    def __repr__(self):
+        return f"BrownianBridge(b={self.b}, t={self.t})"
+
+    @property
+    def b(self):
+        """Right endpoint value."""
+        return self._b
+
+    @b.setter
+    def b(self, value):
+        check_numeric(value, "Right endpoint value")
+        self._b = value
+
+    def _sample_brownian_bridge(self, n, b=None):
+        """Generate a realization of a Brownian bridge."""
+        if b is None:
+            b = self.b
+        times = np.linspace(0, self.t, n + 1)
+        bm = self._sample_brownian_motion(n)
+        bridge = bm + times * (b - bm[-1]) / self.t
+        return bridge
+
+    def _sample_brownian_bridge_at(self, times, b=None):
+        if b is None:
+            b = self.b
+        bm = self._sample_brownian_motion_at(times)
+        bridge = bm + np.array(times) * (b - bm[-1]) / times[-1]
+        return bridge
+
+    def sample(self, n):
+        return self._sample_brownian_bridge(n)
+
+    def sample_at(self, times, b=None):
+        return self._sample_brownian_bridge_at(times, b)
