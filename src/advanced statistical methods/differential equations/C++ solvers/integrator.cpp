bbdeb81b3e1927c22ce51000dece794912fe1867
Nikhil Lalgudi Vaidyanathan <144494028+nikhil-lalgudi@users.noreply.github.com>
2024-07-05 13:02:38 +0530

    cleanup before major commit

---
+++ src/advanced statistical methods/differential equations/C++ solvers/integrator.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include integrator.hpp
+
+using namespace std;
---
+++ src/advanced statistical methods/differential equations/C++ solvers/integrator.hpp
@@ -0,0 +1,8 @@
+#ifndef INTEGRATOR_HPP
+#define INTEGRATOR_HPP
+
+#include "integrator.hpp"
+
+
+
+#endif 
